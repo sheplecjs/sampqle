d3ca8448ecd494818db49e19a5c05afb6e412a0b
sheplecjs <colin.shepley@pm.me>
2022-12-27 06:54:19 +0000

    Initial report query on detractors by product

--- scripts/03_recent_tx_detractors.sql
+++ scripts/03_recent_tx_detractors.sql
@@ -1,28 +1,28 @@
 -- Join transaction information to detractors
 SELECT
     tx.product,
-    tx.cancelled,
+    SUM(tx.number) AS "sales",
     SUM(nps_user_monthly.detractor) AS "detractors"
 FROM
     (
         SELECT
             t.product,
             t.number,
-            t.cancelled,
             t.timestamp,
             TO_CHAR(t.timestamp, 'YYYY-MM') AS "month",
             sessions.user_id
         FROM
             transactions AS t
             LEFT JOIN sessions ON t.session_id = sessions.session_id
+        WHERE
+            t.cancelled = FALSE
     ) as tx
     LEFT JOIN nps_user_monthly ON tx.user_id = nps_user_monthly.user
     AND tx.month = nps_user_monthly.month
 WHERE
     nps_user_monthly.detractor = 1
 GROUP BY
-    tx.product,
-    tx.cancelled
+    tx.product
 ORDER BY
     detractors DESC
 LIMIT
