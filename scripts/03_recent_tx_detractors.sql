8eafec635dcb2828a9dd30bdfa755992c555be58
sheplecjs <colin.shepley@pm.me>
2022-12-27 06:49:45 +0000

    Initial reporting query for product performance via nps

---
+++ scripts/03_recent_tx_detractors.sql
@@ -0,0 +1,29 @@
+-- Join transaction information to detractors
+SELECT
+    tx.product,
+    tx.cancelled,
+    SUM(nps_user_monthly.detractor) AS "detractors"
+FROM
+    (
+        SELECT
+            t.product,
+            t.number,
+            t.cancelled,
+            t.timestamp,
+            TO_CHAR(t.timestamp, 'YYYY-MM') AS "month",
+            sessions.user_id
+        FROM
+            transactions AS t
+            LEFT JOIN sessions ON t.session_id = sessions.session_id
+    ) as tx
+    LEFT JOIN nps_user_monthly ON tx.user_id = nps_user_monthly.user
+    AND tx.month = nps_user_monthly.month
+WHERE
+    nps_user_monthly.detractor = 1
+GROUP BY
+    tx.product,
+    tx.cancelled
+ORDER BY
+    detractors DESC
+LIMIT
+    20;
