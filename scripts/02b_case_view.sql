ca267d83494969b8181a9580ab7589c865bb4a7d
sheplecjs <colin.shepley@pm.me>
2023-01-02 12:25:13 +0000

    Filename change

---
+++ scripts/02b_case_view.sql
@@ -0,0 +1,29 @@
+-- Create a view that categories our nps responses. This query relies on an average in cases where a single user
+-- submits multiple surveys in one month. This view will be useful for building general NPS reporting as well
+-- as individual product performance analysis.
+CREATE
+OR REPLACE VIEW nps_user_monthly AS
+SELECT
+    nps.user,
+    TO_CHAR(nps.timestamp, 'YYYY-MM') AS "month",
+    CASE
+        WHEN AVG(nps.score) >= 9 THEN 1
+        ELSE 0
+    END AS "promoter",
+    CASE
+        WHEN AVG(nps.score) < 9
+        AND AVG(nps.score) >= 7 THEN 1
+        ELSE 0
+    END AS "passaive",
+    CASE
+        WHEN AVG(nps.score) < 7 THEN 1
+        ELSE 0
+    END AS "detractor"
+FROM
+    nps
+GROUP BY
+    month,
+    nps.user
+ORDER BY
+    nps.user,
+    month;
