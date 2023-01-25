953dc6c27bc05e6532b9e64eae750d2c7a18e7ff
sheplecjs <colin.shepley@pm.me>
2023-01-24 18:26:49 -0800

    added binning & histogram query

---
+++ scripts/timeseries/02_binning.sql
@@ -0,0 +1,36 @@
+-- binning of prices and an in-table histogram
+WITH prices AS (
+    SELECT
+        ROUND(MIN(observed), 2) as min_price,
+        ROUND(MAX(observed), 2) as max_price
+    from
+        aud_usd
+),
+hist AS (
+    SELECT
+        WIDTH_BUCKET(observed, min_price, max_price, 9) AS bucket,
+        numrange(
+            ROUND(MIN(observed), 2),
+            ROUND(MAX(observed), 2),
+            '[]'
+        ) AS price_range,
+        count(*) AS freq
+    FROM
+        aud_usd,
+        prices
+    GROUP BY
+        bucket
+    ORDER BY
+        bucket
+)
+SELECT
+    price_range,
+    freq,
+    REPEAT(
+        '|',
+        (
+            freq :: float / MAX(freq) OVER() * 30
+        ) :: int
+    ) AS bar
+FROM
+    hist;
