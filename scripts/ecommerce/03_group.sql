eb2064f1865e3ea2dc64e212bac41d36754bdb99
sheplecjs <colin.shepley@pm.me>
2023-01-24 16:38:40 -0800

    added NPS score calculation

--- scripts/ecommerce/03_group.sql
+++ scripts/ecommerce/03_group.sql
@@ -3,7 +3,19 @@ SELECT
     n.month AS "Month",
     SUM(n.promoter) AS "Promoters",
     SUM(n.passaive) AS "Passaives",
-    SUM(n.detractor) AS "Detractors"
+    SUM(n.detractor) AS "Detractors",
+    ROUND(
+        CAST(SUM(n.promoter) AS DECIMAL) / CAST(
+            (
+                SUM(n.detractor) + SUM(n.passaive) + SUM(n.promoter)
+            ) AS DECIMAL
+        ) - CAST(SUM(n.detractor) AS DECIMAL) / CAST(
+            (
+                SUM(n.detractor) + SUM(n.passaive) + SUM(n.promoter)
+            ) AS DECIMAL
+        ),
+        2
+    ) * 100 AS "Score"
 FROM
     nps_user_monthly AS n
 GROUP BY
