ea5a9c4b6f5535109e4ab72b1fec849d6376c66d
sheplecjs <colin.shepley@pm.me>
2023-02-11 15:27:32 +0000

    fixed table naming in COPY commands

--- scripts/timeseries/00_create.sql
+++ scripts/timeseries/00_create.sql
@@ -1,5 +1,4 @@
 -- drop and create a database for timeseries synthetic data create a table and read in from the variable 'timeseries'
-
 DROP DATABASE IF EXISTS sampqle_timeseries;
 
 CREATE DATABASE sampqle_timeseries;
@@ -10,4 +9,16 @@ CREATE TABLE aud_usd(idx INT PRIMARY KEY, observed DECIMAL, tm DATE);
 
 COPY aud_usd(idx, observed, tm)
 FROM
-    :timeseries DELIMITER ',' CSV HEADER;
+    :timeseries_1 DELIMITER ',' CSV HEADER;
+
+CREATE TABLE gbp_usd(idx INT PRIMARY KEY, observed DECIMAL, tm DATE);
+
+COPY gbp_usd(idx, observed, tm)
+FROM
+    :timeseries_2 DELIMITER ',' CSV HEADER;
+
+CREATE TABLE gbp_aud(idx INT PRIMARY KEY, observed DECIMAL, tm DATE);
+
+COPY gbp_aud(idx, observed, tm)
+FROM
+    :timeseries_3 DELIMITER ',' CSV HEADER;
